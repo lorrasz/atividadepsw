505a3c5c00d59600394855dc77ba3111041ed15b
08324084177 <08324084177@DPSOLADES-07.ifto.local>
2021-05-13 07:55:24 -0300

    Eu estou enviando meu projeto.

---
+++ paginalogin/src/main/java/controller/loginControle.java
@@ -0,0 +1,43 @@
+package controller;
+
+
+	import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+	import javax.inject.Named;
+
+	@Named
+	@RequestScoped
+	public class loginControle implements Serializable {
+		String usuario;
+		String senha;
+		
+		public String validarAcesso() {
+			if("Lorrany".equals(usuario) && "123".equals(senha)) {
+
+			return "menu";	
+			}
+			else {
+			return "login";
+		}
+		}
+		
+		public String getUsuario() {
+			return usuario;	
+		
+
+	}
+		public void setUsuario(String usuario) {
+			this.usuario = usuario;
+		}
+
+		public String getSenha() {
+			return senha;
+			
+		}
+		
+		public void setSenha(String senha) {
+			this.senha = senha;
+		}
+
+}
